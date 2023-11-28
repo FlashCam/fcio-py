135ca9f7c2dc67c7d08c67025f4d9349a916cc38
Simon Sailer <simon.sailer@mpi-hd.mpg.de>
2023-11-28 21:01:36 +0100

    Add gh page generation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,14 +13,14 @@ import inspect
 try:
     import fcio
 except ImportError:
-    raise RuntimeError('Cannot import fcio, please investigate.')
+    raise RuntimeError('Cannot import fcio, which is required to parse the docstrings from cython code.')
 
 # generate docs from the installed source code, might differ from the code in the repo
 # and is required for cython modules
 sys.path.insert(0, os.path.abspath(os.path.dirname(inspect.getfile(fcio))))
 
 project = 'fcio'
-copyright = '2023, Simon Sailer'
+copyright = '2023, FlashCam'
 author = 'Simon Sailer'
 
 # -- General configuration ---------------------------------------------------
@@ -28,9 +28,9 @@ author = 'Simon Sailer'
 
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'sphinx.ext.autosummary',
     'sphinx_mdinclude'
 ]
 napoleon_google_docstring = False
@@ -44,5 +44,11 @@ exclude_patterns = ['_build', '_templates']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_title = 'fcio-py'
 html_static_path = ['_static']
+html_theme = 'furo'
+html_theme_options = {
+    "source_repository": "https://github.com/FlashCam/fcio-py/",
+    "source_branch": "main",
+    "source_directory": "docs/source/",
+}
