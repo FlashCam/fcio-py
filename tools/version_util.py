e7408f8bd9d022949ae94e00dcd6eabf79c558c1
Simon Sailer <simon.sailer@mpi-hd.mpg.de>
2024-07-10 17:20:59 +0200

    Reorder version determination.

--- tools/version_util.py
+++ tools/version_util.py
@@ -1,39 +1,47 @@
 #!/usr/bin/env python3
 
-from git import Repo
-   
+try:
+  from git import Repo
+  GIT_INSTALL_FOUND = True
+except ImportError:
+  GIT_INSTALL_FOUND = False
+
 def get_git_version(cwd):
-    try:
-        repo = Repo(cwd)
-        return repo.git.describe('--tags')
-    except Exception:
-        return None
- 
+  """ Try to get the current version from git tags. """
+  try:
+    repo = Repo(cwd)
+    git_describe = repo.git.describe('--tags')
+    parts = git_describe.split('-')
+    if len(parts) == 1:
+      return parts[0]
+    elif len(parts) == 3:
+      return f"{parts[0]}.dev{parts[1]}"
+    else:
+      return None
+  except Exception:
+    return None
+
 def get_version_from_pkg_info(filename):
-    # Could probably do this with `pkginfo` or something, but this seems to work, too.
-    with open(filename, 'r') as fio:
-        for line in fio:
-            parts = line.split()
-            if parts[0] == "Version:":
-                return parts[1]
+  """ Try to get the current version from a PKG-INFO file. """
+  # Could probably do this with module `pkginfo` or similar, but this seems to work, too.
+  try:
+    with open(filename, 'r', encoding='utf-8') as fio:
+      for line in fio:
+        tokens = line.split()
+        if tokens[0] == "Version:":
+          return tokens[1]
+      return None
+  except FileNotFoundError:
+    return None
 
 if __name__ == "__main__":
-    version_file_path = 'PKG-INFO'
+  PKG_INFO_FILE = 'PKG-INFO'
 
-    git_describe = get_git_version('.')
-    if git_describe:
-        parts = git_describe.split('-')
-        if len(parts) == 1:
-            git_version = parts[0]
-        elif len(parts) == 3:
-            git_version = f"{parts[0]}.dev{parts[1]}"
-        else:
-            print("Unkown")
-            # raise KeyError(f"Parsed git version does not conform the expected number of dashes. Got {git_describe}, but only none or 2 dash-separated format is expected from `git describe`.")
-        print(git_version)
-    else:
-        file_version = get_version_from_pkg_info(version_file_path)
-        if file_version:
-            print(file_version)
-        else:
-            print("Unknown")
+  version = get_version_from_pkg_info(PKG_INFO_FILE)
+  if not version and GIT_INSTALL_FOUND:
+    version = get_git_version('.')
+
+  if version:
+    print(version)
+  else:
+    print("unknown")
